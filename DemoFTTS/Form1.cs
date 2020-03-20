850baf2b89998d9db3e63d259252ec8c9418aa20
LincolnBerlick <lincoln.berlick@gmail.com>
2020-03-20 09:59:47 -0300

    fix

--- DemoFTTS/Form1.cs
+++ DemoFTTS/Form1.cs
@@ -18,14 +18,16 @@ namespace DemoFTTS
             Connect ftts = new Connect();
             int i = FTTSS.Connect.methods.Capturar;
             FTTSS.Connect connect = new Connect();
-<<<<<<< HEAD
+
             String digital = null;
 
             try
             {
-                digital = connect.getDigitalString(FTTSS.Connect.methods.Capturar);
-                txtbox_digital.Text = digital;
                 
+                digital = connect.getDigitalString(FTTSS.Connect.methods.Capturar);
+
+                txtbox_digital.Text = digital;
+
             }
             catch (Exception d)
             {
@@ -37,14 +39,14 @@ namespace DemoFTTS
             lb_text_ip.Text = connect.ambiente.ip;
             lb_text_solicitante.Text = connect.ambiente.user;
             picbox_img_digital.Visible = true;
+            txtbox_digital.Text = digital;
             picbox_img_digital.Image = new System.Drawing.Bitmap(utils.Conversor(utils.conertstringtoTEXTENCODE(digital)), new System.Drawing.Size(picbox_img_digital.Width, picbox_img_digital.Height));
 
         }
 
-        private void Button2_Click(object sender, EventArgs e)
-        {
 
-            // Utils utils = new Utils();
+        private void Button2_Click_1(object sender, EventArgs e)
+        {
             Connect ftts = new Connect();
             int i = FTTSS.Connect.methods.Capturar;
             FTTSS.Connect connect = new Connect();
@@ -64,29 +66,14 @@ namespace DemoFTTS
             lb_text_ip.Text = connect.ambiente.ip;
             lb_text_solicitante.Text = connect.ambiente.user;
             picbox_img_digital.Visible = false;
-            
-=======
-          
-            try
-            {
-                txtbox_digital.Text = connect.getDigitalString(FTTSS.Connect.methods.Capturar);
-
-            }
-            catch(Exception d)
-            {
-                lb_status_text.Text = "Não foi possível capturar encontrar o leitor";
-                MessageBox.Show(d.ToString());
-            }
-
-            lb_status_text.Text = "Coletado com Sucesso" + connect.ambiente.ip;
-            lb_text_ip.Text = connect.ambiente.ip;
 
 
 
->>>>>>> ba662f0a7f944a02a2b1a0270a5fed59f2b97255
 
+        }
 
-
+        private void Label2_Click(object sender, EventArgs e)
+        {
 
         }
     }
